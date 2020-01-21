1d79ca63d915e4224e118e3a6a7881fffaa90209
flower <1228745275@qq.com>
2020-01-21 23:15:33 +0800

    fix bug

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import {get, fileUpload} from "./axios";
+import {get, fileUpload, post} from "./axios";
 
 export const GET_ARTICLES = (para?: any) => get("/articles", para);
 export const GET_PERSON_ACTIVE_RANKINGS = (para?: any) => get("/person_active_rankings", para);
@@ -9,5 +9,5 @@ export const GET_COMMENTS = (para?: any) => get("/comments", para);
 export const GET_NOTICES = (para?: any) => get("/notices", para);
 export const GET_NOTICE_NUM = (para?: any) => get("/notice_num", para);
 export const UPLOAD = (para?: any) => fileUpload("/upload", para);
-export const SET_ARTICLE_SAVE = (para?: any) => get("/save", para);
+export const SET_ARTICLE_SAVE = (para?: any) => post("/save", para);
 export const SET_NOTICE_READ = (para?: any) => get("/notice_read", para);
